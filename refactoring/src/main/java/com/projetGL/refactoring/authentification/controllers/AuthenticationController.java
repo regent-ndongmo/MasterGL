a2f0acfdabd278b622a85318076b8d4763d4401c
Asaph-D <kouokamasaph142@gmail.com>
2024-12-29 05:49:34 +0100

    register

--- refactoring/src/main/java/com/projetGL/refactoring/authentification/controllers/AuthenticationController.java
+++ refactoring/src/main/java/com/projetGL/refactoring/authentification/controllers/AuthenticationController.java
@@ -7,12 +7,14 @@ import com.projetGL.refactoring.authentification.models.RegisterRequest;
 import com.projetGL.refactoring.authentification.regentJwt.services.AuthenticationService;
 import com.projetGL.refactoring.authentification.regentJwt.services.JwtService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class AuthenticationController {
 
