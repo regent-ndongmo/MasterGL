2e58aae7ec736b15011de97d0620e6b06c971f52
Asaph-D <kouokamasaph142@gmail.com>
2025-01-04 10:36:40 +0100

    linter

--- refactoring/src/test/java/com/projetGL/refactoring/RefactoringApplicationTests.java
+++ refactoring/src/test/java/com/projetGL/refactoring/RefactoringApplicationTests.java
@@ -2,7 +2,6 @@ package com.projetGL.refactoring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class RefactoringApplicationTests {
 
